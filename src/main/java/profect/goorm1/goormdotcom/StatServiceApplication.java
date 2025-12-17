4f24f646a71041f0e992fc48ddf60b90333b9c19
ThinkKat <jwy06048@gmail.com>
2025-12-17 16:22:09 +0900

    :tada: initial setting

---
+++ src/main/java/profect/goorm1/goormdotcom/StatServiceApplication.java
@@ -0,0 +1,13 @@
+package profect.goorm1.goormdotcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/profect/goorm1/goormdotcom/StatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package profect.goorm1.goormdotcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
