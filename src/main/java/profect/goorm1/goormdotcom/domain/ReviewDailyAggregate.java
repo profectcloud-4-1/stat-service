2b4abfcef470a9bea2c4e11457df11800080482f
ThinkKat <jwy06048@gmail.com>
2025-12-21 14:44:21 +0900

    :recycle: #1 [refactor] update aggregate class to record class

--- src/main/java/profect/goorm1/goormdotcom/domain/ReviewDailyAggregate.java
+++ src/main/java/profect/goorm1/goormdotcom/domain/ReviewDailyAggregate.java
@@ -1,19 +1,12 @@
 package profect.goorm1.goormdotcom.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class ReviewDailyAggregate {
-
-    private LocalDate statDate;
-    private UUID productId;
-    private long reviewCount;
-    private long ratingSum;
+public record ReviewDailyAggregate (
+    LocalDate statDate,
+    UUID productId,
+    long reviewCount,
+    long ratingSum
+) {
 }
